6a67083c80d06f76cd1faa01de2bacf65d12fc8e
uhlitz <uhlitz@gmail.com>
2016-03-19 15:04:34 +0100

    updated manual

--- R/idconv.R
+++ R/idconv.R
@@ -7,8 +7,8 @@
 #' @param ids vector of input identifiers to be converted to output identifiers
 #' @return A vector of converted identifiers with same length as input identifiers
 #' @examples
-#' SYMBOL_to_ENTREZ(c("EGR1", "FOS"))
-#' ENTREZ_to_SYMBOL(c("1958", "2353"))
+#' SYMBOL_to_ENTREZID(c("EGR1", "FOS"))
+#' ENTREZID_to_SYMBOL(c("1958", "2353"))
 #' SYMBOL_to_ENSEMBL(c("EGR1", "FOS"))
 #' ENSEMBL_to_SYMBOL(c("ENSG00000120738", "ENSG00000170345"))
 #' @export
@@ -21,8 +21,26 @@ IDX_to_IDY <- function(IDX, IDY, ids) {
     } %>%
     .[ids]
 }
+#' @describeIn IDX_to_IDY convert from SYMBOL to ENTREZID
+#' @export
+SYMBOL_to_ENTREZID  <- function(ids) IDX_to_IDY("SYMBOL", "ENTREZID", ids = ids)
 
-SYMBOL_to_ENTREZ  <- function(ids) IDX_to_IDY("SYMBOL", "ENTREZID", ids = ids)
-ENTREZ_to_SYMBOL  <- function(ids) IDX_to_IDY("ENTREZID", "SYMBOL", ids = ids)
-ENSEMBL_to_SYMBOL <- function(ids) IDX_to_IDY("ENSEMBL", "SYMBOL", ids = ids)
+#' @describeIn IDX_to_IDY convert from ENTREZID to SYMBOL
+#' @export
+ENTREZID_to_SYMBOL  <- function(ids) IDX_to_IDY("ENTREZID", "SYMBOL", ids = ids)
+
+#' @describeIn IDX_to_IDY convert from SYMBOL to ENSEMBL
+#' @export
 SYMBOL_to_ENSEMBL <- function(ids) IDX_to_IDY("SYMBOL", "ENSEMBL", ids = ids)
+
+#' @describeIn IDX_to_IDY convert from ENSEMBL to SYMBOL
+#' @export
+ENSEMBL_to_SYMBOL <- function(ids) IDX_to_IDY("ENSEMBL", "SYMBOL", ids = ids)
+
+#' @describeIn IDX_to_IDY convert from SYMBOL to REFSEQ
+#' @export
+SYMBOL_to_REFSEQ <- function(ids) IDX_to_IDY("SYMBOL", "REFSEQ", ids = ids)
+
+#' @describeIn IDX_to_IDY convert from REFSEQ to SYMBOL
+#' @export
+REFSEQ_to_SYMBOL <- function(ids) IDX_to_IDY("REFSEQ", "SYMBOL", ids = ids)
