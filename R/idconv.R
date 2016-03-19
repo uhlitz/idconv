11096c88964f574af95cff05e2a15b635c040130
uhlitz <uhlitz@gmail.com>
2016-03-19 15:46:51 +0100

    changed order of arguments in IDX_to_IDY function to be compatible with piped operations

--- R/idconv.R
+++ R/idconv.R
@@ -2,9 +2,9 @@
 #'
 #' \code{IDX_to_IDY} converts ids from identifier X to identifier Y using AnnotationDbi
 #'
-#' @param IDX input id type (e.g. "SYMBOL")
-#' @param IDY output id type (e.g. "ENSEMBL")
-#' @param ids vector of input identifiers to be converted to output identifiers
+#' @param from source id type (e.g. "SYMBOL")
+#' @param to target id type (e.g. "ENSEMBL")
+#' @param ids vector of source identifiers to be converted to target identifiers
 #' @return A vector of converted identifiers with same length as input identifiers
 #' @examples
 #' SYMBOL_to_ENTREZID(c("EGR1", "FOS"))
@@ -12,34 +12,34 @@
 #' SYMBOL_to_ENSEMBL(c("EGR1", "FOS"))
 #' ENSEMBL_to_SYMBOL(c("ENSG00000120738", "ENSG00000170345"))
 #' @export
-IDX_to_IDY <- function(IDX, IDY, ids) {
+IDX_to_IDY <- function(ids, from, to) {
   id_df <- AnnotationDbi::select(org.Hs.eg.db::org.Hs.eg.db, keys = unique(ids),
-                                 columns = c(IDX, IDY),
-                                 keytype = IDX)
-  id_df <- id_df[match(unique(id_df[, IDX]), id_df[, IDX]), ]
-  setNames(id_df[, IDY], id_df[, IDX])[ids]
+                                 columns = c(from, to),
+                                 keytype = from)
+  id_df <- id_df[match(unique(id_df[, from]), id_df[, to]), ]
+  setNames(id_df[, to], id_df[, from])[ids]
 }
 
 #' @describeIn IDX_to_IDY convert from SYMBOL to ENTREZID
 #' @export
-SYMBOL_to_ENTREZID  <- function(ids) IDX_to_IDY("SYMBOL", "ENTREZID", ids = ids)
+SYMBOL_to_ENTREZID  <- function(ids) IDX_to_IDY(ids = ids, "SYMBOL", "ENTREZID")
 
 #' @describeIn IDX_to_IDY convert from ENTREZID to SYMBOL
 #' @export
-ENTREZID_to_SYMBOL  <- function(ids) IDX_to_IDY("ENTREZID", "SYMBOL", ids = ids)
+ENTREZID_to_SYMBOL  <- function(ids) IDX_to_IDY(ids = ids, "ENTREZID", "SYMBOL")
 
 #' @describeIn IDX_to_IDY convert from SYMBOL to ENSEMBL
 #' @export
-SYMBOL_to_ENSEMBL <- function(ids) IDX_to_IDY("SYMBOL", "ENSEMBL", ids = ids)
+SYMBOL_to_ENSEMBL <- function(ids) IDX_to_IDY(ids = ids, "SYMBOL", "ENSEMBL")
 
 #' @describeIn IDX_to_IDY convert from ENSEMBL to SYMBOL
 #' @export
-ENSEMBL_to_SYMBOL <- function(ids) IDX_to_IDY("ENSEMBL", "SYMBOL", ids = ids)
+ENSEMBL_to_SYMBOL <- function(ids) IDX_to_IDY(ids = ids, "ENSEMBL", "SYMBOL")
 
 #' @describeIn IDX_to_IDY convert from SYMBOL to REFSEQ
 #' @export
-SYMBOL_to_REFSEQ <- function(ids) IDX_to_IDY("SYMBOL", "REFSEQ", ids = ids)
+SYMBOL_to_REFSEQ <- function(ids) IDX_to_IDY(ids = ids, "SYMBOL", "REFSEQ")
 
 #' @describeIn IDX_to_IDY convert from REFSEQ to SYMBOL
 #' @export
-REFSEQ_to_SYMBOL <- function(ids) IDX_to_IDY("REFSEQ", "SYMBOL", ids = ids)
+REFSEQ_to_SYMBOL <- function(ids) IDX_to_IDY(ids = ids, "REFSEQ", "SYMBOL")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # idconv
 
-An R package to convert hg19 (GRCh37) identifiers
+Wrapper functions to convert gene identifiers available from the org.Hs.eg.db package.
 
 # Installation  
 
@@ -8,12 +8,14 @@ An R package to convert hg19 (GRCh37) identifiers
 devtools::install_github("uhlitz/idconv")
 ```
 
-# Example
+# Examples
 
 ```
 library(idconv)
-SYMBOL_to_ENTREZ(c("EGR1", "FOS"))
-ENTREZ_to_SYMBOL(c("1958", "2353"))
+SYMBOL_to_ENTREZID(c("EGR1", "FOS"))
+ENTREZID_to_SYMBOL(c("1958", "2353"))
 SYMBOL_to_ENSEMBL(c("EGR1", "FOS"))
 ENSEMBL_to_SYMBOL(c("ENSG00000120738", "ENSG00000170345"))
+IDX_to_IDY(ids = "NM_005252", from = "REFSEQ", to = "SYMBOL")
 ```
+
